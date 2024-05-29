cdc0c4ff16b056f862bac199deda59573bf7b6b6
Niklas Minth <niklas.minth@gmail.com>
2024-05-29 22:02:55 +0200

    dashboard application setup

---
+++ dashboard/src/Themes.ts
@@ -0,0 +1,19 @@
+
+export const lightTheme = {
+    bgColorPrimary: '#ffffff',
+    bgColorSecondary: '#212922',
+    bgColorTertiary: '#5B8266',
+    textColorPrimary: '#000028',
+    textColorSecondary: '#3E6259',
+    textColorTertiary:  '#3b3b3b'
+}
+
+export const darkTheme = {
+    bgColorPrimary: '#ffffff',
+    bgColorSecondary: '#212922',
+    bgColorTertiary: '#5B8266',
+    textColorPrimary: '#000028',
+    textColorSecondary: '#3E6259',
+    textColorTertiary: '#3b3b3b'
+    
+}
---
+++ dashboard/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { OverviewComponent } from './pages/overview/overview.component';
+import { ResultsComponent } from './pages/results/results.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: OverviewComponent
+  },
+  {
+    path: 'results',
+    component: ResultsComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
