8b1c3e4a746ae5a997f1d2b9c65e50999b9f0eee
Ruben Kaiser <ruben@kaiser.fyi>
2024-06-10 16:04:42 +0200

    refactor: formatting

--- src/models/models.py
+++ src/models/models.py
@@ -1,7 +1,6 @@
-from dataclasses import dataclass
-from models.base import Column,BaseModel
+from base import Column, BaseModel
 
-from typing import List,Optional
+from typing import List, Optional
 
 from sqlalchemy import JSON, ForeignKey, Integer
 
@@ -11,34 +10,34 @@ from sqlalchemy import String
 
 from sqlalchemy.orm import relationship
 
+
 class Recommendation(BaseModel):
     __tablename__ = "recommendations"
     recommendation: str = Column(String, nullable=False)
     generic: bool = Column(String, default=False, nullable=False)
-    finding_id: int = Column(Integer, ForeignKey("findings.id"),nullable=True)
+    finding_id: int = Column(Integer, ForeignKey("findings.id"), nullable=True)
     finding: Mapped[Optional['Finding']] = relationship('Finding', uselist=False, back_populates='recommendations')
-    
+
     def __repr__(self):
         return f"<Recommendation {self.recommendation}>"
-    
-    
+
+
 class Finding(BaseModel):
     __tablename__ = "findings"
     finding = Column(String, default=None, nullable=True)
     cve = Column(String, default=None, nullable=True)
     priority = Column(String, default=None, nullable=True)
     source = Column(String, default=None, nullable=True)
-    content  = Column(JSON, default=None, nullable=False)
+    content = Column(JSON, default=None, nullable=False)
     # recommendations: list["Recommendation"] = Relationship(back_populates="finding")
-    recommendations : Mapped[List['Recommendation']]  = relationship('Recommendation', back_populates='finding')
-    
-    
-    def __init__(self,finding:str,  content:JSON):
+    recommendations: Mapped[List['Recommendation']] = relationship('Recommendation', back_populates='finding')
+
+    def __init__(self, finding: str, content: JSON):
         self.content = content
 
     def __repr__(self):
         return f"<Finding {self.finding}>"
-    
+
 
 class User(BaseModel):
     __tablename__ = "users"
