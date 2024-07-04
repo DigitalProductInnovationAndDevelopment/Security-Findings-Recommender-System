034d9bba7c1421809a8bad2c7b9d9dc647390d5b
Ruben Kaiser <ruben@kaiser.fyi>
2024-07-05 00:02:08 +0200

    feat: category class with str and to_dict

--- src/data/Categories.py
+++ src/data/Categories.py
@@ -21,6 +21,7 @@ class SecurityAspect(Enum):
     INPUT_VALIDATION = "InputValidation"
     XSS = "CrossSiteScripting"
     SQL_INJECTION = "SQLInjection"
+    CommandInjection = "CommandInjection"
 
 
 class SeverityLevel(Enum):
@@ -50,15 +51,57 @@ class Compliance(Enum):
     PCI_DSS = "PCI_DSS"
     HIPAA = "HIPAA"
 
+class Environment(Enum):
+    DOCKER = "Docker"
+    KUBERNETES = "Kubernetes"
+    DIRECT = "Direct"
+
 # class Location # TODO: Add Location class
 
 # TODO: add kmeans clustering for categories based on solution
 
 
 class Category(BaseModel):
-    technology_stack: Optional[List[TechnologyStack]] = Field(default_factory=list)
-    security_aspect: Optional[List[SecurityAspect]] = Field(default_factory=list)
+    technology_stack: Optional[List[TechnologyStack]] = None
+    security_aspect: Optional[List[SecurityAspect]] = None
     severity_level: Optional[SeverityLevel] = None
-    remediation_type: Optional[List[RemediationType]] = Field(default_factory=list)
-    affected_component: Optional[List[AffectedComponent]] = Field(default_factory=list)
-    compliance: Optional[List[Compliance]] = Field(default_factory=list)
+    remediation_type: Optional[List[RemediationType]] = None
+    affected_component: Optional[List[AffectedComponent]] = None
+    compliance: Optional[List[Compliance]] = None
+    environment: Optional[List[Environment]] = None
+
+    def __str__(self):
+        my_str = ""
+        if self.technology_stack:
+            my_str += f"Technology Stack: {self.technology_stack.value}\n"
+        if self.security_aspect:
+            my_str += f"Security Aspect: {self.security_aspect.value}\n"
+        if self.severity_level:
+            my_str += f"Severity Level: {self.severity_level.value}\n"
+        if self.remediation_type:
+            my_str += f"Remediation Type: {self.remediation_type.value}\n"
+        if self.affected_component:
+            my_str += f"Affected Component: {self.affected_component.value}\n"
+        if self.compliance:
+            my_str += f"Compliance: {self.compliance.value}\n"
+        if self.environment:
+            my_str += f"Environment: {self.environment.value}\n"
+        return my_str
+
+    def to_dict(self):
+        my_dict = {}
+        if self.technology_stack:
+            my_dict["technology_stack"] = self.technology_stack.value
+        if self.security_aspect:
+            my_dict["security_aspect"] = self.security_aspect.value
+        if self.severity_level:
+            my_dict["severity_level"] = self.severity_level.value
+        if self.remediation_type:
+            my_dict["remediation_type"] = self.remediation_type.value
+        if self.affected_component:
+            my_dict["affected_component"] = self.affected_component.value
+        if self.compliance:
+            my_dict["compliance"] = self.compliance.value
+        if self.environment:
+            my_dict["environment"] = self.environment.value
+        return my_dict
